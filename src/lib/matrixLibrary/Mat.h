4a0adaad59596cec575b44cc16373f9fc1690f55
Ahmed Atallah <aero.ahmed.atallah@gmail.com>
2021-02-18 08:57:45 -0800

    updated matrix library

--- src/lib/matrixLibrary/Mat.h
+++ src/lib/matrixLibrary/Mat.h
@@ -1,61 +1,40 @@
 #ifndef _MAT_H_
 #define _MAT_H_
 
-// all the system #include's we'll ever need
 #include <iostream>
-
-
-
 using namespace std;
 
-
 class Mat {
-
 public:
-
-
 	// Cosntructors
 	Mat();
 	Mat(int n);		        // Zero-based array
 	Mat(int m, int n);		// Zero-based array
-
 	Mat(int m, const double &a);	        //initialize to constant value
-
 	Mat(int m, int n , const double &a);	//initialize to constant value
-
 	Mat(int m, const double *a);        	// Initialize to array
 	Mat(int m, const int *a);        	// Initialize to array
 	Mat(int m, int n , const double *a);	// Initialize to array
-
 	Mat(double x, double y, double z);   // 3dim-Vector
 	Mat (double x, double y, double z,   // 6dim-Vector
 			double X, double Y, double Z);
 	// Assignement
 	Mat(const Mat &rhs);	// Copy constructor
-
 	Mat( double Inital, double Final, int N);
-
 	Mat operator=(const Mat& rhs);	//assignment
-
 	Mat(const Mat &A, const Mat &B);   // Assemble two matrices
-
 
 	friend Mat Eye(const int n);      // Identity Matrix
 
 	typedef double value_type; // make T available externally
 
 	// Accessing Elements
-	//inline double & operator()(const int i);	//i'th element
-	// inline double & operator()(const int i, const int j);	//i'th and j'th element
 	double  operator () (int i) const { return v_[i]; };
 	double& operator () (int i)       { return v_[i]; };
-
 	double  operator () (int i,int j) const { return v_[i*n_cols_+j]; };
 	double& operator () (int i,int j)       { return v_[i*n_cols_+j]; };
 
-
 	Mat operator ~ ();
-
 	Mat Transpose();
 
 	// Scalar multiplication and division of a vector
@@ -73,20 +52,14 @@ public:
 	// Unary negative
 	friend Mat operator - (const Mat& V);
 
-	// Mat & operator*(const double & value);
-	//friend Mat operator*(const double& dFactor, const Mat& oMatrix) ;
-
 	// Size
 	inline int Size() const;
-	//void resize(int newn); // resize (contents not preserved)
-	//void assign(int newn, double &a); // resize and assign a constant value
-	//void assign(int newn, double &a);
 
 	// Minimum and Maximum Values
 	double Min();
 	double Max();
 
-	// // Dot product, norm, cross product
+	// Dot product, norm, cross product
 	double Dot(const Mat &rhs);
 	double Norm();
 	Mat    Abs();
@@ -107,7 +80,6 @@ public:
 	// Concatenation 
 	friend Mat Assemble(const Mat& a, const Mat& b);
 
-
 	friend Mat Get(const Mat &A, const double min , const double max);
 	// Elementary rotations
 	friend Mat R_x(double Angle);
@@ -115,32 +87,26 @@ public:
 	friend Mat R_z(double Angle);
 
 	// Sorting
-
 	void Sort();
 	void Sort(Mat & rhs);
-	//Get floati
-
+	
 	float* Getf();
 	// Output
 	friend std::ostream& operator << (std::ostream& os, const Mat& Matrix);
 
 	// Solve Linear System Using Gauss
-
-
 	Mat GaussLin(const Mat rhs,int n) const;
 
 	Mat Inverse() const;
-	// Direct Product 
 
+	// Direct Product 
 	Mat DirectProduct(const Mat &rhs) const;
 
 	// resize
 	void Resize(int newn);
-
 	void Resize(int nColsNew ,int nRowsNew);
 	// Destructor
 	~Mat();
-
 
 	// Variables
 	int n_rows_;	// size of array. upper index is nn-1
